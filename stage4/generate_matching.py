167f66de21944ca48d8fee658fed995e965f7995
KanWu <kanwu@cs.wisc.edu>
2017-04-16 11:44:58 -0500

    finish merging

---
+++ stage4/generate_matching.py
@@ -0,0 +1,26 @@
+# get two entities, how to merge them into the final entity in E
+import sys
+import csv
+import re
+
+
+
+if __name__ == "__main__":
+	# read in two entities
+	ids = open('predicted_values_ids.csv')
+	reader_ids = csv.DictReader(ids)
+	
+	file_1 = open('anime_1.csv')
+	file_2 = open('animePlayer.csv')
+	file_1 = file_1.readlines()
+	file_2 = file_2.readlines()
+
+	out_1 = open('anime_1_match.csv','w')
+	out_2 = open('animePlayer_match.csv','w')
+	out_1.write('ID,Title,Episodes,ProductionHouse,Genres,Type,Year,Rating,ProductionHouse_Merged,Genres_Merged\n')
+	out_2.write('ID,Title,Episodes,ProductionHouse,Genres,Type,Year,Rating,ProductionHouse_Merged,Genres_Merged\n')
+	# fetch two tuples
+	for row in reader_ids:
+		out_1.write(file_1[int(row['ltable_ID'])])
+		out_2.write(file_2[int(row['rtable_ID'])])
+	# write into two tables
--- stage4/merge.py
+++ stage4/merge.py
@@ -3,14 +3,6 @@ import sys
 import csv
 import re
 
-#def filter_fields(entity_l, entity_r):
-#	fields = [entity_l.split(","), entity_r.split(",")]
-#	return fields
-
-
-def operate_miss(field):
-	return 0
-
 def union(set1, set2):
 	# transfer to set
 		# filter []
@@ -23,19 +15,15 @@ def union(set1, set2):
 	set2 = set2.split(",")
 	# union
 		# union
-	set_merged = list(set(set1) | set(set2))
-		# add []
-	# TODO return string?
-	#print("passed in set1 " + str(set1))
-	#print("passed in set2 " + str(set2))
-	#print("try to print " + str(type(set_merged)) + " : " + str(set_merged))
+	set_merged = str(list(set(set1) | set(set2)))
+	set_merged = re.sub(r'''"''', '', set_merged)
 	return set_merged
 
 def judge_integer(string_to_test):
 	if(string_to_test.isdigit()):
 		return int(string_to_test)
 	if(string_to_test == '?'):
-		return 3000
+		return 2017
 	return -1
 
 def split(year):
@@ -49,7 +37,7 @@ def split(year):
         # do something special
 	year[1]= re.sub(r' ', '', year[1])
 	
-	# find illegal return 0
+	# find illegal return -1
 	year[0] = judge_integer(year[0])
 	year[1] = judge_integer(year[1])
 
@@ -73,14 +61,14 @@ def union_year(year1, year2):
 		else:
 			startyear = startyear2
 	if (startyear == -1):
-		startyear = 'Unkown'
+		startyear = 'Unknown'
 		
 		# end
 	if (endyear1 > endyear2):
 		endyear = endyear1
 	else:
 		endyear = endyear2
-	if (endyear==3000):
+	if (endyear==2017):
 		endyear = '?'
 
 	return str(startyear) + '-' + str(endyear)
@@ -89,18 +77,32 @@ def merge(entity_1, entity_player):
 	result = []
 		
 		# Title: longer one (no miss)
+	print(entity_1['Title'])
 	if (len(entity_1['Title']) > len(entity_player['Title'])):
 		result.append(entity_1['Title'])
 	else:
 		result.append(entity_player['Title'])
 		
 		# Episodes: choose larger one
-	# TODO miss?  + ???? 
-	#operate_miss('Episodes') 
-	if ( int(entity_1['Episodes']) > int(entity_player['Episodes']) ):
-		result.append(entity_1['Episodes'])
+	tmp1 = re.sub(r'\+', '', str(entity_1['Episodes']))
+	tmp2 = re.sub(r'\+', '', str(entity_player['Episodes']))
+	if (tmp1.isdigit()==False):
+		tmp1 = 'Unknown'
+	if (tmp2.isdigit()==False):
+		tmp2 = 'Unknown'
+	if (tmp1 == 'Unknown'):
+		if (tmp2 == 'Unknown'):
+			result.append('Unknown')
+		else:
+			result.append(entity_player['Episodes'])
 	else:
-		result.append(entity_player['Episodes'])
+		if (tmp2 == 'Unknown'):
+			result.append(entity_1['Episodes'])
+		else:
+			if ( int(tmp1) > int(tmp2) ):
+				result.append(entity_1['Episodes'])
+			else:
+				result.append(entity_player['Episodes'])
 
 		# ProductionHouse merge!
 	result.append(union(entity_1['ProductionHouse'], entity_player['ProductionHouse']))
@@ -113,7 +115,7 @@ def merge(entity_1, entity_player):
 		result.append(entity_player['Type'])
 	else:
 		if (entity_1['Type']==''):
-			result.append('Unkown')
+			result.append('Unknown')
 		else:
 			result.append(entity_1['Type'])
 
@@ -121,12 +123,11 @@ def merge(entity_1, entity_player):
 	result.append(union_year(entity_1['Year'], entity_player['Year']))
 
 		# Rating : ainme_1
-	result.append(entity_1['Rating'])
 	if(entity_1['Rating']!=''):
 		result.append(entity_1['Rating'])
 	else:
 		if (entity_player['Rating']==''):
-			result.append('Unkown')
+			result.append('Unknown')
 		else:
 			result.append(entity_player['Rating'])
 	
@@ -136,23 +137,34 @@ def merge(entity_1, entity_player):
 if __name__ == "__main__":
 	# read in two entities
 	
-	csv_1 = open('animetry_1.csv')
-	csv_player = open('animetry_Player.csv')
+	csv_1 = open('anime_1_match.csv')
+	csv_player = open('animePlayer_match.csv')
 	reader_1 = csv.DictReader(csv_1)
 	reader_player = csv.DictReader(csv_player)
-	entity_1 = next(reader_1)
-	entity_player = next(reader_player)
-	
-	print("l: " + str(entity_1))
-	print()
-	print("r: " + str(entity_player))
-	
-	# get each field
-	print()
-	# use rules to generate each field
 
-	after_merge = merge(entity_1, entity_player)
+	out = open('merged_overall.csv', 'w')
+	out.write('ID,Title,Episodes,ProductionHouse,Genres,Type,Year,Rating\n')
+	for i in range(5124):
+		entity_1 = next(reader_1)
+		entity_player = next(reader_player)
+		print("===== iteration " + str(i))
+		print(entity_1)
+		print()
+		print(entity_player)
 	
+		print()
+		# use rules to generate each field
 
-	# return the new entity
-	print("the result we get: \n" + str(after_merge))
+		#after_merge = str(i+1) + ',' + str(merge(entity_1, entity_player)) + '\n'
+		after_merge = str(i+1)
+		merged = merge(entity_1, entity_player)
+		after_merge += ''', "''' + str(merged[0]) + '''", ''' + str(merged[1]) + ''', "''' + str(merged[2]) + '''", "''' + str(merged[3]) + '''", ''' + str(merged[4]) + ', ' + str(merged[5]) + ', ' + str(merged[6]) + '\n'
+		out.write(after_merge)
+		print("the result we get: \n" + str(after_merge))
+
+
+
+
+
+
+
